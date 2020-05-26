2fd429e27210f28c54554008f936e8ea27989ddf
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2020-05-26 13:02:47 +0100

    Functional carry box scetch complete

--- Carry_box/Carry_box.ino
+++ Carry_box/Carry_box.ino
@@ -15,11 +15,14 @@ byte addresses[][6] = {"1Node","2Node"};              // Radio pipe addresses fo
 
 
 void setup() {
+  Serial.begin(9600);
+  printf_begin();
+  Serial.println("begin");
   // setup interupt
   pinMode(3, INPUT_PULLUP);
-  attachInterrupt(1, hit_detect, FALLING)
+  attachInterrupt(1, detect_hit, FALLING);
   // setup transmission
-
+  Serial.println("st");
   radio.begin();
   radio.setAutoAck(1);                    // Ensure autoACK is enabled
   radio.enableAckPayload();               // Allow optional ack payloads
@@ -30,35 +33,46 @@ void setup() {
   radio.startListening();                 // Start listening
   radio.powerUp();
   radio.printDetails();                   // Dump the configuration of the rf unit for debugging
-  
+  Serial.println("est");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //delay(1000);
+  //Serial.println(!digitalRead(3));
+  //Serial.println(millis() - hit_time);
+  if(hit_confirmed && (micros() - hit_time) > 1000 && !digitalRead(3)){
+    if(transmit_hit(1)){
+      hit_confirmed = false;
+    }
+  }
 
 }
 
-void hit_detect(){
-  
+void detect_hit(){
+  hit_time = micros();
+  hit_confirmed = true;
 }
 
-bool transmit_hit(){
+bool transmit_hit(byte message){
+  bool success = false;
   byte gotByte;                                           // Initialize a variable for the incoming response
     
   radio.stopListening();                                  // First, stop listening so we can talk.      
-  printf("Now sending %d as payload. ",1);                // Use a simple byte counter as payload
+  printf("Now sending %d as payload. ",message);                // Use a simple byte counter as payload
   unsigned long time = micros();                          // Record the current microsecond count   
-                                                          
-  if ( radio.write(&counter,1) ){                         // Send the counter variable to the other radio 
+  
+  if ( radio.write(&message,1) ){                         // Send the counter variable to the other radio 
       if(!radio.available()){                             // If nothing in the buffer, we got an ack but it is blank
           printf("Got blank response. round-trip delay: %lu microseconds\n\r",micros()-time);     
       }else{      
           while(radio.available() ){                      // If an ack with payload was received
               radio.read( &gotByte, 1 );                  // Read it, and display the response time
               printf("Got response %d, round-trip delay: %lu microseconds\n\r",gotByte,micros()-time);
-              counter++;                                  // Increment the counter variable
+              success = true;
           }
       }
   
   }else{        printf("Sending failed.\n\r"); }          // If no ack response, sending failed
+  
+  return success;
 }
